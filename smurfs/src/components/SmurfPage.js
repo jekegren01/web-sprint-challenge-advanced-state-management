a67c2e7e270a91bdac8f38426ad791a59ff04fe7
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-08-30 18:38:15 -0700

    create smurfpage.js

---
+++ smurfs/src/components/SmurfPage.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {connect} from "react-redux";
+import {getSmurf} from "../actions";
+
+const SmurfPage = props => {
+
+    const {id} = useParams();
+    
+    useEffect(()=>{
+        props.getSmurf(id);
+    }, [])
+
+    return (
+        <div className="smurf-page">
+            {props.smurf && 
+                <div>
+                    <h1>{props.smurf.name}</h1>
+                    <p>Age: {props.smurf.age}</p>
+                    <p>Height: {props.smurf.height}</p>
+                </div>
+            }
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return  {
+        smurf: state.smurf
+    }
+}
+
+const mapDispatchToProps = {getSmurf};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SmurfPage);
